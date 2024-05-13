7c84f0c4fd4952f8786ea8be31701ee322b8594f
Boris Verkhovskiy <boris.verk@gmail.com>
2024-05-12 19:27:54 -0600

    Persist selected language hightlight

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,13 @@
 (function() {
   document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('lang-select').addEventListener('change', function() {
-      var lang = this.value;
+    var langSelect = document.getElementById('lang-select');
+    if (!langSelect) {
+      return
+    }
+
+    function set_lang(lang) {
+      langSelect.value = lang
+
       var links = document.querySelectorAll('td.lang a');
       links.forEach(function(link) {
         if (link.textContent === lang) {
@@ -10,6 +16,26 @@
           link.classList.remove('selected');
         }
       });
+    }
+
+    var LANG_KEY = "lxiym_lang";
+    function load_lang() {
+      var lang = localStorage.getItem(LANG_KEY);
+      if (lang) {
+        set_lang(lang);
+      }
+    }
+
+    load_lang();
+    // Backup: do it onload. Will blink, but better than nothing.
+    window.onload = function(){
+      load_lang();
+    };
+
+    langSelect.addEventListener('change', function() {
+      var lang = this.value;
+      set_lang(lang);
+      localStorage.setItem(LANG_KEY, lang);
     });
   });
 }).call(this);
